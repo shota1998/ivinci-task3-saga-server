45c2b5aaf8ac65b3a9a1bd80fab492716d91a4a3
Shota Sato <sugergg@gmail.com>
2021-06-10 10:01:55 +0900

    first commit

---
+++ db/crud/createPost.js
@@ -0,0 +1,27 @@
+const { v4: uuidv4 } = require('uuid');
+const AWS = require('aws-sdk');
+
+AWS.config.update({
+  endpoint: 'http://localhost:8000',
+  region: 'us-west-2',
+  accessKeyId: 'fakeMyKeyId',
+  secretAccessKey: 'fakeSecretAccessKey',
+});
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+const params = {
+  TableName: 'Post',
+  Item: {
+    id: uuidv4(),
+    title: 'title1',
+    body: 'body1',
+  },
+};
+
+docClient.put(params, (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
---
+++ db/crud/updatePost.js
@@ -0,0 +1,29 @@
+const AWS = require('aws-sdk');
+
+AWS.config.update({
+  endpoint: 'http://localhost:8000',
+  region: 'us-west-2',
+  accessKeyId: 'fakeMyKeyId',
+  secretAccessKey: 'fakeSecretAccessKey',
+});
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+const params = {
+  TableName: 'Post',
+  Key: {
+    id: 'b8b97083-4f31-4f98-acd8-f739db2b0e35',
+  },
+  UpdateExpression: 'set title = :t, body = :b',
+  ExpressionAttributeValues: {
+    ':t': 'update title',
+    ':b': 'update body',
+  },
+};
+
+docClient.update(params, (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
